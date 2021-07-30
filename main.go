0bf039dc49c27e764e89b3b3cfd9070ab4f94943
Patrick Lubach <patrick.lubach@wabion.com>
2021-07-30 08:30:23 +0200

    introduce viper cmd

--- main.go
+++ main.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"os"
+	"caligula/cmd"
 )
 
-type Health struct {
-	Status string `json:"status"`
-}
+// type Health struct {
+// 	Status string `json:"status"`
+// }
 
-func getJson(url string, target interface{}) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		os.Exit(1)
-	}
-	return json.NewDecoder(resp.Body).Decode(target)
-}
+// func getJson(url string, target interface{}) error {
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+// 	if resp.StatusCode != 200 {
+// 		os.Exit(1)
+// 	}
+// 	return json.NewDecoder(resp.Body).Decode(target)
+// }
 
 func main() {
-	data := new(Health)
-	getJson("http://localhost:8080/admin/health", data)
-	if data.Status != "UP" {
-		os.Exit(1)
-	}
+	cmd.Execute()
+	// data := new(Health)
+	// getJson("http://localhost:8080/admin/health", data)
+	// if data.Status != "UP" {
+	// 	os.Exit(1)
+	// }
 }
